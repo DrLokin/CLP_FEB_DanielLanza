5107a1763dac51779896a9a6504f964d203d78e6
DrLokin <daniel.lanzav@gmail.com>
2023-02-13 13:59:37 -0500

    Initializing the back end application

---
+++ clp-backend/src/main/java/com/daniellanza/clpbackend/ClpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daniellanza.clpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClpBackendApplication.class, args);
+	}
+
+}
---
+++ clp-backend/src/test/java/com/daniellanza/clpbackend/ClpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniellanza.clpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
